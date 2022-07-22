02ee92081edfbdefbcd23a9766168a9a84a34789
Ben Faerber <benfaerber@protonmail.com>
2022-07-22 04:34:25 -0600

    Cleaned up the lexer

--- liquid_parser/lexer.ml
+++ liquid_parser/lexer.ml
@@ -8,9 +8,9 @@ let lex_bool text =
   let literal_false = "false" in
 
   if starts_with text literal_true then
-    Some(LexValue (LexBool true), remove_prefix text literal_true)
+    Some (LexValue (LexBool true), remove_prefix text literal_true)
   else if starts_with text literal_false then
-    Some(LexValue (LexBool false), remove_prefix text literal_false)
+    Some (LexValue (LexBool false), remove_prefix text literal_false)
   else
     None
 
@@ -27,7 +27,7 @@ let lex_digit_group text =
   lex_digit_group_aux text [] |> join
 
 let lex_number text =
-  let to_num v t = Some (LexValue (LexNumber(v |> Float.of_string)), t) in
+  let to_num v t = Some (LexValue (LexNumber (Float.of_string v)), t) in
 
   let (neg_literal, t_text) =
     if starts_with text "-" then
@@ -36,7 +36,7 @@ let lex_number text =
 
   match lex_digit_group t_text with
   | "" -> None
-  | first_group -> (
+  | first_group -> begin
     let t_first_group = neg_literal ^ first_group in
     let decimal_part = remove_prefix t_text first_group in
     if starts_with decimal_part "." then
@@ -46,7 +46,7 @@ let lex_number text =
       | second_group -> to_num (t_first_group ^ "." ^ second_group) (remove_prefix second_group_part second_group)
     else
       to_num t_first_group decimal_part
-  )
+  end
 
 let has_prefix_or_fail text prefix func =
   if starts_with text prefix then
@@ -57,12 +57,12 @@ let has_prefix_or_fail text prefix func =
 let lex_range text =
   let (popen, pclose) = "(", ")" in
   let dotdot = ".." in
-  has_prefix_or_fail text popen (fun wo_paren ->
+  let lex_first_group wo_paren =
     match lex_digit_group wo_paren with
     | "" -> None
     | first_number ->
       let after_first = remove_prefix wo_paren first_number in
-      has_prefix_or_fail after_first dotdot (fun wo_dot ->
+      let lex_second_group wo_dot =
         match lex_digit_group wo_dot with
         | "" -> None
         | second_number ->
@@ -72,8 +72,12 @@ let lex_range text =
             Some (range, (remove_prefix after_second pclose))
           else
             None
-      )
-  )
+      in
+
+      has_prefix_or_fail after_first dotdot lex_second_group
+  in
+
+  has_prefix_or_fail text popen lex_first_group
 
 
 let lex_delimited_string delim escaped_delim text =
@@ -84,8 +88,9 @@ let lex_delimited_string delim escaped_delim text =
       | e when e = escaped_delim -> Next (acc ^ escaped_delim, index + d_len)
       | other -> (
         match first_letter other with
-        | e when e = delim -> Stop (acc)
-        | other_letter -> Next (acc ^ other_letter, index + 1))
+        | e when e = delim -> Stop acc
+        | other_letter -> Next (acc ^ other_letter, index + 1)
+      )
     in
 
     let string_literal = unfold "" 0 folder in
@@ -105,16 +110,6 @@ let lex_string text =
   | _ -> None
 
 
-let rec first_successful (text: string) =
-  function
-  | lexer :: other_lexers -> (
-    match lexer text with
-    | Some (got, rest) -> Some (got, rest)
-    | _ -> first_successful text other_lexers
-  )
-  | _ -> None
-
-let (~/) = Re2.create_exn
 let lex_id text =
   let id_exp = ~/"^[a-zA-Z_](?:(?:[a-zA-Z0-9_\\-\\.]|((\\[(\"|\')?.+)(\"|\')?\\]))+)?" in
   if Re2.matches id_exp text then
@@ -123,12 +118,13 @@ let lex_id text =
     let wo_bg =
       match Re2.find_all bracket_group_exp text with
       | Ok mats ->
-        List.fold mats ~init:literal ~f:(fun acc m -> (
+        let folder acc m =
           let is_string = starts_with m "[\"" || starts_with m "['" in
           let pos = if is_string then 2 else 1 in
           let dot_notation = "." ^ String.sub m ~pos:pos ~len:(String.length m - (pos * 2)) in
           String.substr_replace_first acc ~pattern:m ~with_:dot_notation
-        ))
+        in
+        List.fold mats ~init:literal ~f:folder
       | Error _ -> literal in
 
     let pieces = String.split wo_bg ~on:'.' in
@@ -136,10 +132,32 @@ let lex_id text =
   else
     None
 
+
+let rec first_successful text =
+  function
+  | lexer :: other_lexers -> (
+    match lexer text with
+    | Some p -> Some p
+    | _ -> first_successful text other_lexers
+  )
+  | _ -> None
+
 let lex_token text =
   let lexers = [lex_keyword; lex_range; lex_bool; lex_string; lex_number; lex_id] in
   first_successful text lexers
 
+
+let lex_block_token_chunk chunk acc index =
+  if is_block_token_string chunk then
+    Next (acc @ [block_token_of_string chunk], index+(String.length chunk))
+  else
+    match List.rev acc with
+    | (RawText "liquid") :: StatementStart :: hds ->
+      Next (List.rev hds @ [LiquidStart], index+1)
+    | (RawText tl) :: hds ->
+      Next (List.rev hds @ [RawText (tl ^ first_letter chunk)], index+1)
+    | _ ->
+      Next (acc @ [RawText (first_letter chunk)], index+1)
 
 let lex_block_tokens text =
   let folder acc index =
@@ -148,23 +166,11 @@ let lex_block_tokens text =
       let raw_text = Preprocessor.until_end_raw curr in
       let raw_body = Preprocessor.trim_raw_tags raw_text in
       Next (acc @ [RawText raw_body], index+(String.length raw_text))
+    else if index + 2 < (String.length text) then
+      let chunk = String.sub text ~pos:index ~len:2 in
+      lex_block_token_chunk chunk acc index
     else
-      if index + 2 < (String.length text) then
-        let chunk = String.sub text ~pos:index ~len:2 in
-        match chunk with
-        | c when is_block_token_string c ->
-          Next (acc @ [block_token_of_string chunk], index+(String.length c))
-        | other -> (
-          match acc |> List.rev with
-          | RawText("liquid") :: StatementStart :: hds ->
-            Next ((List.rev hds) @ [LiquidStart], index+1)
-          | RawText(tl) :: hds ->
-            Next (List.rev hds @ [RawText(tl ^ first_letter other)], index+1)
-          | _ ->
-            Next (acc @ [RawText(first_letter other)], index+1)
-        )
-      else
-        Stop(acc)
+      Stop acc
   in
 
   unfold [] 0 folder
@@ -173,14 +179,15 @@ let lex_line_tokens text =
   let t_text = text ^ " " in
   let folder acc index =
     let chunk = String.sub t_text ~pos:index ~len:(String.length t_text - index) in
+    let sub_rest rest = String.length t_text - String.length rest in
     match lex_token chunk with
-    | Some(Newline, rest) -> Next (acc @ [EOS; Newline], String.length t_text - String.length rest)
-    | Some(t, rest) -> Next (acc @ [t], String.length t_text - String.length rest)
-    | _ -> Stop (acc)
+    | Some (Newline, rest) -> Next (acc @ [EOS; Newline], sub_rest rest)
+    | Some (t, rest) -> Next (acc @ [t], sub_rest rest)
+    | _ -> Stop acc
   in
 
   let raw_list = unfold [] 0 folder in
-  let wo_spaces = List.filter raw_list ~f:(fun token -> token != Space) in
+  let wo_spaces = List.filter raw_list ~f:((!=) Space) in
 
   wo_spaces
 
@@ -208,10 +215,14 @@ let lex_all_tokens (block_tokens: block_token list) =
       | ExpressionStart :: RawText(body) :: ExpressionEnd :: _ ->
         Next (acc @ [LexExpression (lex_line_tokens body)], index+3)
       | LiquidStart :: RawText(body) :: StatementEnd :: _ ->
-        let liq = body |> Preprocessor.remove_liquid_comments |> lex_line_tokens in
+        let liq = body
+          |> Preprocessor.remove_liquid_comments
+          |> lex_line_tokens in
         Next (acc @ liq, index+3)
       | RawText (other) :: _ ->
-        Next (acc @ [Newline; LexText (other)], index + 1)
+        (* There was once a newline here *)
+        (* [Newline; LexText other] *)
+        Next (acc @ [LexText other], index + 1)
       | _ -> Stop (acc)
     end
   in
